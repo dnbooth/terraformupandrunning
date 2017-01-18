3d2b7940f3a091cc49be38a11544c388cc7e77ed
Dave Booth <dave.booth@cdl.co.uk>
2017-01-18 16:01:23 +0000

    various changes

--- main.tf
+++ main.tf
@@ -1,11 +1,29 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
+  profile = "cdlpoc"
 }
 
 resource "aws_instance" "dbooth-example" {
-  ami           = "ami-40d28157"
+  ami           = "ami-0d77397e"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
   tags {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
